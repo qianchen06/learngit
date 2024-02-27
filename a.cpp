d659ce6c6278df0052ee55c00aab9a07fcbf2da6
qianchen <1660956797@qq.com>
2024-02-27 21:00:23 +0800

    sub

---
+++ a.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+git remote add origin 链接
+添加远程仓库
+
+git remote -v 查看远程仓库
+
+git push remote origin 推到远程仓库
+
+git add xxx .
+
+git commit -m 
+
+*/
+signed main()
+{
+
+}
